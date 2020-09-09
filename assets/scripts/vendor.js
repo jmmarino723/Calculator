126c0e4483b6c4aba7fae2463d9d6cdc64b7f8c7
jmmarino723 <69175253+jmmarino723@users.noreply.github.com>
2020-09-08 19:48:10 -0500

    Update vendor.js

--- assets/scripts/vendor.js
+++ assets/scripts/vendor.js
@@ -1,6 +1,6 @@
 const userInput = document.getElementById('input-number');
 // const addBtn = document.getElementById('btn-add');
-const addBtn = document.querySelector('btn-add');
+const addBtn = document.querySelector("div.calc-actions   button[id='btn-add']");
 const subtractBtn = document.getElementById('btn-subtract');
 const multiplyBtn = document.getElementById('btn-multiply');
 const divideBtn = document.getElementById('btn-divide');
