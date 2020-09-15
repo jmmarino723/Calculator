75e05c45b033c81f46fe9d3979dad15d65f57f87
jmmarino723 <69175253+jmmarino723@users.noreply.github.com>
2020-09-14 20:17:42 -0500

    Add boostrap elements

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,26 +6,35 @@ let result='';
 let cient=false;
 
 function ejecutar(){
-    result=userInput.value+'=';
-    //historial.push(userInput.value);
-    userInput.value=eval(String(userInput.value));
+    result=historyInput.value+'=';
+    userInput.value=eval(String(historyInput.value));
     result+=userInput.value;
     historial.push(result);
+    let newHistory=document.createElement("LI");
+    const totalHistory=document.getElementById("groupHistory");
+    newHistory.className="list-group-item";
+    newHistory.innerText=result;
+    totalHistory.append(newHistory);
 }
 
 function clear(){
     currentResult=0;
+    historyInput.value="";
     userInput.value="";
 }
 function hist(){
     alert(historial);
 }
 function cientifica(){
-    let textCient = ['%','^','pi','(','sqr','log','ln',')'];
+    let textCient = ['sin','cos','tan','(','sqr','log','ln',')'];
     let contenedor='calc-actions';
+    let contenedor2='btn';
     if (cient===false){
         for (let i=0; i<textCient.length; i++){
             let boton = document.createElement('button');
+            boton.className="btn btn-dark";
+            contenedor2=contenedor2+textCient[i];
+            boton.id=contenedor2;
             let aux=i+1;
             if (i<=3){
                 contenedor=contenedor+aux;
@@ -41,10 +50,27 @@ function cientifica(){
                 digitar(boton.innerText);
             })
             contenedor='calc-actions';
+            contenedor2='btn';
         }
         cient=true;
     }
     
 }
 
+function standar(){
+    let textCient = ['sin','cos','tan','(','sqr','log','ln',')'];
+    let contenedor2='btn';
+    if (cient===true){
+        for (let i=0; i<textCient.length; i++){
+            contenedor2=contenedor2+textCient[i];
+            const par1 = document.getElementById(contenedor2);
+            let father=par1.parentNode;
+            father.removeChild(par1);
+            contenedor2='btn';
+        }
+        cient=false;
+    }
+}
+
 scrollBtn.addEventListener('click',cientifica);
+standBtn.addEventListener('click',standar);
